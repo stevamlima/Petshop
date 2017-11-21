d4d32307886754c78d3b4fa4a1e2a30c396452e1
Jessica <jessica.jdss@gmail.com>
2017-11-21 19:09:39 -0200

    adicionando Produtos e Serviços

--- Pet/Dominio/ClassePai/ClassesFilhas/Produtos.cs
+++ Pet/Dominio/ClassePai/ClassesFilhas/Produtos.cs
@@ -1,7 +1,34 @@
+using System;
+
 namespace Pet.Dominio.ClassePai.ClassesFilhas
 {
-    public class Produtos
+    public class Produtos : Vendas
     {
+        public int IdProd {get; set; }
 
+        public string TipoProd { get; set; }
+
+        public int QuantidadeProd { get; set; }
+
+        public double PrecoProd {get ; set;}
+
+
+        public Produtos()
+        {
+            
+        }
+
+        public Produtos(int IdVenda, string DescricaoVenda, DateTime DataVenda, double PrecoVenda, int IdProd, string TipoProd, int QuantidadeProd, double PrecoProd)
+        {
+                base.IdVenda = IdVenda;
+                base.DescricaoVenda = DescricaoVenda;
+                base.DataVenda = DataVenda;
+                base.PrecoVenda = PrecoVenda;
+                this.IdProd = IdProd;
+                this.TipoProd = TipoProd;
+                this.QuantidadeProd = QuantidadeProd;
+                this.PrecoProd = PrecoProd;
+
+        }
     }
 }
